2b2905b25667d7db55aa8703ef7114f40b7fe306
xzg5101 <guoxiaoyang11@outlook.com>
2023-04-11 11:56:15 -0400

    Update base.py

--- nn/layers/base.py
+++ nn/layers/base.py
@@ -63,35 +63,38 @@ class BaseLayer(nn.Module):
 
 
 class MAB(nn.Module):
-    """https://github.com/juho-lee/set_transformer/blob/master/modules.py"""
-
-    # todo: check bias here
     def __init__(self, dim_Q, dim_K, dim_V, num_heads, ln=False):
         super(MAB, self).__init__()
         self.dim_V = dim_V
         self.num_heads = num_heads
-        self.fc_q = nn.Linear(dim_Q, dim_V)
-        self.fc_k = nn.Linear(dim_K, dim_V)
-        self.fc_v = nn.Linear(dim_K, dim_V)
+        self.fc_q = nn.Linear(dim_Q, dim_V, bias=False)
+        self.fc_k = nn.Linear(dim_K, dim_V, bias=False)
+        self.fc_v = nn.Linear(dim_K, dim_V, bias=False)
+        self.fc_o = nn.Linear(dim_V, dim_V, bias=False)
         if ln:
             self.ln0 = nn.LayerNorm(dim_V)
             self.ln1 = nn.LayerNorm(dim_V)
-        self.fc_o = nn.Linear(dim_V, dim_V)
 
     def forward(self, Q, K):
         Q = self.fc_q(Q)
         K, V = self.fc_k(K), self.fc_v(K)
 
-        dim_split = self.dim_V // self.num_heads
-        Q_ = torch.cat(Q.split(dim_split, 2), 0)
-        K_ = torch.cat(K.split(dim_split, 2), 0)
-        V_ = torch.cat(V.split(dim_split, 2), 0)
+        # Multi-head split
+        Q = Q.view(Q.size(0), Q.size(1), self.num_heads, -1).permute(0, 2, 1, 3)
+        K = K.view(K.size(0), K.size(1), self.num_heads, -1).permute(0, 2, 1, 3)
+        V = V.view(V.size(0), V.size(1), self.num_heads, -1).permute(0, 2, 1, 3)
 
-        A = torch.softmax(Q_.bmm(K_.transpose(1, 2)) / math.sqrt(self.dim_V), 2)
-        O = torch.cat((Q_ + A.bmm(V_)).split(Q.size(0), 0), 2)
-        O = O if getattr(self, "ln0", None) is None else self.ln0(O)
+        A = torch.softmax(Q @ K.transpose(-2, -1) / math.sqrt(self.dim_V), dim=-1)
+        O = A @ V
+
+        # Concatenate multi-head outputs
+        O = O.permute(0, 2, 1, 3).contiguous().view(Q.size(0), -1, self.dim_V)
+
+        if hasattr(self, "ln0"):
+            O = self.ln0(O)
         O = O + F.relu(self.fc_o(O))
-        O = O if getattr(self, "ln1", None) is None else self.ln1(O)
+        if hasattr(self, "ln1"):
+            O = self.ln1(O)
         return O
 
 
