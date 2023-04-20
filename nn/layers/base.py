81ab09eb806795c1e1f3ac567c14e492467d147a
xzg5101 <guoxiaoyang11@outlook.com>
2023-04-20 14:02:30 -0400

    Update base.py

--- nn/layers/base.py
+++ nn/layers/base.py
@@ -76,10 +76,8 @@ class BaseLayer(nn.Module):
         return x
 
 
-class MAB(nn.Module):
-    """https://github.com/juho-lee/set_transformer/blob/master/modules.py"""
 
-    # todo: check bias here
+class MAB(nn.Module):
     def __init__(self, dim_Q, dim_K, dim_V, num_heads, ln=False):
         super(MAB, self).__init__()
         self.dim_V = dim_V
@@ -87,6 +85,7 @@ class MAB(nn.Module):
         self.fc_q = nn.Linear(dim_Q, dim_V)
         self.fc_k = nn.Linear(dim_K, dim_V)
         self.fc_v = nn.Linear(dim_K, dim_V)
+        self.attention = nn.MultiheadAttention(dim_V, num_heads)
         if ln:
             self.ln0 = nn.LayerNorm(dim_V)
             self.ln1 = nn.LayerNorm(dim_V)
@@ -94,15 +93,11 @@ class MAB(nn.Module):
 
     def forward(self, Q, K):
         Q = self.fc_q(Q)
-        K, V = self.fc_k(K), self.fc_v(K)
+        K = self.fc_k(K)
+        V = self.fc_v(K)
 
-        dim_split = self.dim_V // self.num_heads
-        Q_ = torch.cat(Q.split(dim_split, 2), 0)
-        K_ = torch.cat(K.split(dim_split, 2), 0)
-        V_ = torch.cat(V.split(dim_split, 2), 0)
-
-        A = torch.softmax(Q_.bmm(K_.transpose(1, 2)) / math.sqrt(self.dim_V), 2)
-        O = torch.cat((Q_ + A.bmm(V_)).split(Q.size(0), 0), 2)
+        A, _ = self.attention(Q, K, V)
+        O = Q + A
         O = O if getattr(self, "ln0", None) is None else self.ln0(O)
         O = O + F.relu(self.fc_o(O))
         O = O if getattr(self, "ln1", None) is None else self.ln1(O)
