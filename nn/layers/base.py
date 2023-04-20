5b685cf314239fff6781faa83bf0fd935cdfe20b
xzg5101 <guoxiaoyang11@outlook.com>
2023-04-20 14:16:01 -0400

    Update base.py

--- nn/layers/base.py
+++ nn/layers/base.py
@@ -66,35 +66,35 @@ class BaseLayer(nn.Module):
 
 
 class MAB(nn.Module):
+    """https://github.com/juho-lee/set_transformer/blob/master/modules.py"""
+
+    # todo: check bias here
     def __init__(self, dim_Q, dim_K, dim_V, num_heads, ln=False):
         super(MAB, self).__init__()
         self.dim_V = dim_V
         self.num_heads = num_heads
-        
-        self.linears = nn.ModuleList([nn.Linear(dim_Q, dim_V),
-                                       nn.Linear(dim_K, dim_V),
-                                       nn.Linear(dim_K, dim_V),
-                                       nn.Linear(dim_V, dim_V)])
-
-        self.multihead_attn = nn.MultiheadAttention(dim_V, num_heads)
-        
+        self.fc_q = nn.Linear(dim_Q, dim_V)
+        self.fc_k = nn.Linear(dim_K, dim_V)
+        self.fc_v = nn.Linear(dim_K, dim_V)
         if ln:
             self.ln0 = nn.LayerNorm(dim_V)
             self.ln1 = nn.LayerNorm(dim_V)
-        else:
-            self.ln0 = None
-            self.ln1 = None
+        self.fc_o = nn.Linear(dim_V, dim_V)
 
     def forward(self, Q, K):
-        Q = self.linears[0](Q)
-        K, V = self.linears[1](K), self.linears[2](K)
+        Q = self.fc_q(Q)
+        K, V = self.fc_k(K), self.fc_v(K)
 
-        A, _ = self.multihead_attn(Q, K, V)
+        dim_split = self.dim_V // self.num_heads
+        Q_ = torch.cat(Q.split(dim_split, 2), 0)
+        K_ = torch.cat(K.split(dim_split, 2), 0)
+        V_ = torch.cat(V.split(dim_split, 2), 0)
 
-        O = Q + A
-        O = O if self.ln0 is None else self.ln0(O)
-        O = O + F.relu(self.linears[3](O))
-        O = O if self.ln1 is None else self.ln1(O)
+        A = torch.softmax(Q_.bmm(K_.transpose(1, 2)) / math.sqrt(self.dim_V), 2)
+        O = torch.cat((Q_ + A.bmm(V_)).split(Q.size(0), 0), 2)
+        O = O if getattr(self, "ln0", None) is None else self.ln0(O)
+        O = O + F.relu(self.fc_o(O))
+        O = O if getattr(self, "ln1", None) is None else self.ln1(O)
         return O
 
 
