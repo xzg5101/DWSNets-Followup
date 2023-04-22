87a8ea313a3eb77b13a3e7dd9f7f5d5542d2f1c4
xzg5101 <guoxiaoyang11@outlook.com>
2023-04-22 14:54:58 -0400

    Update weight_to_weight.py

--- nn/layers/weight_to_weight.py
+++ nn/layers/weight_to_weight.py
@@ -241,6 +241,8 @@ class SetKroneckerSetLayer(BaseLayer):
         shapes = x.shape
         bs = shapes[0]
 
+        out_all = self.lin_layers[0](x.view(bs, self.d1, self.d2, self.in_features))
+        
         x = x.permute(0, 3, 1, 2).flatten(start_dim=2)
         pooled_all = self._reduction(x, dim=2)
         pooled_all = pooled_all.unsqueeze(1).unsqueeze(1)
@@ -248,10 +250,9 @@ class SetKroneckerSetLayer(BaseLayer):
         pooled_rows = self._reduction(x, dim=1, keepdim=True)
         pooled_cols = self._reduction(x, dim=2, keepdim=True)
 
-        out_all = self.lin_layers[0](x)
-        out_rows = self.lin_layers[1](pooled_rows)
-        out_cols = self.lin_layers[2](pooled_cols)
-        out_both = self.lin_layers[3](pooled_all)
+        out_rows = self.lin_layers[1](pooled_rows.view(bs, 1, self.d2, self.in_features))
+        out_cols = self.lin_layers[2](pooled_cols.view(bs, self.d1, 1, self.in_features))
+        out_both = self.lin_layers[3](pooled_all.view(bs, 1, 1, self.in_features))
 
         new_features = out_all * 0.25 + out_rows * 0.25 + out_cols * 0.25 + out_both * 0.25
         return new_features
